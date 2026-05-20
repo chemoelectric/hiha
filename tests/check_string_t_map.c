725b66da9e9157f35f1cd20d4b6ca8611fb9e52b
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2026-05-20 10:56:59 -0500

    string_t_map deletion and tests for it. Also more thorough tests of insertion.

--- tests/check_string_t_map.c
+++ tests/check_string_t_map.c
@@ -41,6 +41,18 @@ int_negate (int n)
   return -n;
 }
 
+static int
+int_negate_if_even (int n)
+{
+  return ((n % 2) == 0) ? -n : n;
+}
+
+static bool
+int_is_anything (int n)
+{
+  return true;
+}
+
 static bool
 int_is_odd (int n)
 {
@@ -117,25 +129,40 @@ delete_from_map (string_t_map_t map, bool (*pred) (int))
 }
 
 static void
-check_map (string_t_map_t map, int (*f) (int))
+check_map (string_t_map_t map, bool (*pred) (int), int (*f) (int))
 {
   for (int i = 1; i != HOW_MANY + 1; i += 1)
-    {
-      char buf[100];
-      snprintf (buf, 100, "key%06d", i);
-      string_t key = make_string_t (buf);
-      const void *value = string_t_map_search (map, key);
-      assert (value != NULL);
-      assert (*((const int *) value) == f (i));
-    }
+    if (pred (i))
+      {
+        char buf[100];
+        snprintf (buf, 100, "key%06d", i);
+        string_t key = make_string_t (buf);
+        const void *value = string_t_map_search (map, key);
+        assert (value != NULL);
+        assert (*((const int *) value) == f (i));
+      }
   for (int i = 1; i != HOW_MANY + 1; i += 1)
     {
       char buf[100];
-      snprintf (buf, 100, "yek%06d", i);
+      snprintf (buf, 100, "no-such-key-%06d", i);
       string_t key = make_string_t (buf);
       const void *value = string_t_map_search (map, key);
       assert (value == NULL);
     }
+}
+
+static void
+check_missing (string_t_map_t map, bool (*pred) (int))
+{
+  for (int i = 1; i != HOW_MANY + 1; i += 1)
+    if (pred (i))
+      {
+        char buf[100];
+        snprintf (buf, 100, "key%06d", i);
+        string_t key = make_string_t (buf);
+        const void *value = string_t_map_search (map, key);
+        assert (value == NULL);
+      }
 }
 
 int
@@ -150,22 +177,46 @@ main (void)
 
   map = insert_into_map (map, &int_identity);
   assert (string_t_map_size (map) == HOW_MANY);
-  check_map (map, &int_identity);
+  check_map (map, &int_is_anything, &int_identity);
   map = insert_into_map (map, &int_negate);
   assert (string_t_map_size (map) == HOW_MANY);
-  check_map (map, &int_identity);
+  check_map (map, &int_is_anything, &int_identity);
   map = replace_in_map (map, &int_negate);
   assert (string_t_map_size (map) == HOW_MANY);
-  check_map (map, &int_negate);
+  check_map (map, &int_is_anything, &int_negate);
   map = insert_into_or_replace_in_map (map, &int_identity);
   assert (string_t_map_size (map) == HOW_MANY);
-  check_map (map, &int_identity);
+  check_map (map, &int_is_anything, &int_identity);
 
   string_t_map_t map_even = delete_from_map (map, &int_is_odd);
   assert (2 * string_t_map_size (map_even) == HOW_MANY);
   assert (string_t_map_size (map) == HOW_MANY);
+  check_map (map_even, &int_is_even, &int_identity);
+  check_missing (map_even, &int_is_odd);
+
   string_t_map_t map_odd = delete_from_map (map, &int_is_even);
   assert (2 * string_t_map_size (map_odd) == HOW_MANY);
+  assert (string_t_map_size (map) == HOW_MANY);
+  check_map (map_odd, &int_is_odd, &int_identity);
+  check_missing (map_odd, &int_is_even);
+
+  string_t_map_t map_empty = delete_from_map (map, &int_is_anything);
+  assert (string_t_map_size (map_empty) == 0);
+  assert (map_empty == NULL);
+
+  string_t_map_t map2 = insert_into_map (map_odd, &int_negate);
+  assert (string_t_map_size (map2) == HOW_MANY);
+  check_map (map2, &int_is_anything, &int_negate_if_even);
+
+  string_t_map_t map3 =
+    insert_into_or_replace_in_map (map_odd, &int_negate);
+  assert (string_t_map_size (map3) == HOW_MANY);
+  check_map (map3, &int_is_anything, &int_negate);
+
+  string_t_map_t map4 = replace_in_map (map_odd, &int_negate);
+  assert (2 * string_t_map_size (map4) == HOW_MANY);
+  check_map (map4, &int_is_odd, &int_negate);
+  check_missing (map4, &int_is_even);
 
   return 0;
 }
